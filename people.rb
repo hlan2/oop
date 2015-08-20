71d6f193694d398f70d522883fcca47548b7cd29
Henry Lan <henrylan@Henrys-MacBook-Air.local>
2015-08-20 13:51:15 -0400

    First attempt at exercise 1

---
+++ people.rb
@@ -0,0 +1,28 @@
+class Person(name)
+	def initialize(name)
+		@name = name
+	end
+
+	puts "Hi my name is #{name}"
+
+	class Student
+	
+		def learn 
+		puts "I get it!"
+		end
+	end
+
+	class Instructor
+
+		def teach
+		puts "Everything in Ruby is an Object."
+		end
+
+	end
+end
+
+student = Person.new("Chris")
+instructor = person.new("Christina")
+
+student.Student
+instructor.Instructor
